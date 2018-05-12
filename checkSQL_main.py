22fc3181fa667e11354c81eaadee3ee1ed729dcc
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-05-12 11:51:25 +0200

    Update checkSQL_main.py
    Now program can exit if the user doesn't want to keep looking at data. Removed comments to reduce code.

--- checkSQL_main.py
+++ checkSQL_main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 10 12:29:05 2018
-
 @author: airos
 
 Does a quick check of SQL databases and tables.
@@ -14,7 +12,6 @@ requires 'checkSQL_db.py' to be in same working directory
 """
 
 from checkSQL_db_edit import Find_SQL_DB, Explore_SQL, Explore_Data, User_Input
-
 
 def show_options(datacont_instance):
     data_cont = datacont_instance.datacont_type
@@ -31,7 +28,6 @@ def getDataCont_Name(input_instance,datacont_instance):
     data_cont = di.datacont_type
     while ii.stop == False:
         ii.text = input('Please enter the number corresponding to the {}: '.format(data_cont))
-        #checks the input and gets database name
         datacont_name, ii.stop = ii.str2index(di.item_list)
     return(datacont_name)
     
@@ -50,38 +46,23 @@ def no_items(data_container1, data_container2):
     print("\n!! No %s found in %s !!\n" % (data_container1,data_container2))
     return None
     
-    
 if __name__ == '__main__':
-    #first collect database names in current working directory (i.e. '.db' files)
     dbs = Find_SQL_DB()
-    #dbs_list = dbs.db_list
     if dbs.item_list:
-        #exp_databases = True
         while dbs.stop == False:
-            #database_entry = False
-            
-            #list databases with a number for user to choose which to work with
             show_options(dbs)
             db_input = User_Input()
             db_name = getDataCont_Name(db_input,dbs)
-                
-            #establishes connection with SQL database via sqlite3
-            #then list tables for user to choose which to work with
             currdb = Explore_SQL(db_name)
             tables = currdb.tables2list()
             if tables:
                 while currdb.stop == False:
-                    #list tables in database for user to choose from
                     show_options(currdb)
                     table_input = User_Input()
                     table_name = getDataCont_Name(table_input,currdb)
-                    
-                    #converts table data to pandas df
                     df = currdb.table2dataframe(table_name)
-                    #conduct calculations and "learn" about data in table
                     currdf = Explore_Data(df)
                     currdf.print_profile(table_name)
-                    
                     if currdf.depvar_numunique == 1:
                         currdf.stop = True
                     while currdf.stop == False:
@@ -94,17 +75,24 @@ if __name__ == '__main__':
                             dv_input = User_Input()
                             dv_name = getDataCont_Name(dv_input,currdf)
                             if dv_input.stop == True:
-                                currdf.print_profile(table_name, dv_name)
-                                
+                                currdf.print_profile(table_name, dv_name)        
                     if len(tables) == 1:
                             currdb.stop == True
                             break
-                    
+                    else:
+                        yes_no = stop_OR_go(currdb.datacont_type)
+                        if 'no' in yes_no:
+                            currdb.stop = True
+                            break  
             else:
                 no_items('tables','database')
             if len(dbs.item_list) == 1:
                 dbs.stop = True
+            else:
+                yes_no = stop_OR_go(dbs.datacont_type)
+                if 'no' in yes_no:
+                    dbs.stop = True
+                    break
     else:
         no_items('databases','directory')
-
     currdb.close_conn_NOsave()
